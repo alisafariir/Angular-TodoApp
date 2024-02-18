6c150bc00ab6d627dff90d607c3c72e9a582930a
Ali Safari <asafari72@gmail.com>
2024-02-18 21:39:54 +0330

    end binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,19 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FormsModule],
   standalone: true,
 })
-export class AppComponent {}
+export class AppComponent {
+  name = 'Ali Safari';
+
+  id = 'u-0';
+
+  username = 'alisafari';
+}
